762a9df00f53c25cc4cf3fcd005dade0e5d389a5
lin0110 <jiajunlin2016@gmail.com>
2019-11-05 21:33:05 +0000

    Finish

--- js/script.js
+++ js/script.js
@@ -3,30 +3,35 @@ $( "document" ).ready(function() { //this helps your jQuery to work. Write all o
 
 
 $("#button").click(function() {
+    $(".output").text("");
 
-var userInput = $(".input").val();
+    var userInput = $(".input").val();
+    var words = userInput.split(' ');
+    var vowels = ["a", "e", "i", "o", "u"];
 
-var words = userInput.split(' ');
-
-words.forEach(function(word){
-    var letter = word.charAt(0)
-    if (letter != "a" || letter != "e" || letter != "i" || letter != "o" || letter != "u"){
-
-        $(".output").text( word.slice(1) + letter + "ay");
+    words.forEach(function(word){
+        var letter = word.charAt(0).toLowerCase();
+        if (vowels.includes(letter)){
+            var vowel = word + "ay ";
+            $(".output").append(vowel);
+        }
+        else{
+            var constants =word.slice(1) + letter + "ay ";
+            $(".output").append(constants);
     }
-    else{
-        $(".output").text( word.slice(1) + letter + "ay");
-    }
-}
-
-
 });
 
-
-
-
-
-
+/* let words = song.split(" ");
+    let beg1 = words[0].charAt(0);
+    let beg2 = words[1].charAt(0);
+    console.log(beg1);
+    console.log(beg2);
+    let end1 = words[0].slice(1, words[0].length);
+    let end2 = words[1].slice(1, words[1].length);
+    console.log(end1);
+    console.log(end2);
+    $("#result").append(end1+beg1+" "+end2+beg2);
+    */
 
 
 });
